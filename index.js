2ad81f5d00ef94be5264808a40333f888f45d7f0
Khadeeja59 <kkausar59@gmail.com>
2021-12-03 02:39:22 +0000

    added nextISSTimesForMyLocation function call

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
-const coords = { latitude: 45.4446, longitude: -75.6385 };
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+const nextISSTimesForMyLocation  = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// const coords = { latitude: 45.4446, longitude: -75.6385 };
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
-fetchCoordsByIP('174.114.14.84', (error, location)=> {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+//   console.log('It worked! Returned IP:' , ip);
+// });
+// fetchCoordsByIP('174.114.14.84', (error, location)=> {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned location:' , location);
+// });
+
+// fetchISSFlyOverTimes( coords, (error, arrPasses)=> {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times' , arrPasses);
+// });
+
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log('It worked! Returned location:' , location);
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
 
-fetchISSFlyOverTimes( coords, (error, arrPasses)=> {
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-  console.log('It worked! Returned flyover times' , arrPasses);
+  // success, print out the deets!
+  console.log(passTimes);
 });
 
 
